b4b96bee3b895b2a02cc2ff56f597cff3aa2de6a
Louisa Smith <louisahsmith@gmail.com>
2021-07-27 15:57:00 -0400

    prepare for heroku

--- run.R
+++ run.R
@@ -1,21 +1,6 @@
-# obtain port argument (if given)
-args <- commandArgs(trailingOnly = TRUE)
 
-if (length(args) == 0) {
-  port <- "12000"
-} else if (length(args) == 1) {
-  port <- args[1]
-}
-
-print(paste("Listening on port", port))
-
+port <- Sys.getenv('PORT')
 
 rmarkdown::run(dir = getwd(),
-               shiny_args = list(host = "127.0.0.1",
+               shiny_args = list(host = '0.0.0.0',
                                  port = as.numeric(port)))
-
-# shiny::runApp(
-#   appDir = getwd(),
-#   host = "127.0.0.1",
-#   port = as.numeric(port)
-# )
